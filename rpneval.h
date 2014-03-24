fe3a797dfba2d4f75bcf19e3c9262fefd40107b0
U-Pidyon\yochi <yochi@Pidyon.(none)>
2014-03-23 23:17:50 -0400

    second commit

---
+++ rpneval.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define STACKSIZE 265
+
+
+double pop(void);
+void push(double x);
+void printstack(void);
+int eval_rpn(double *result, char *expr);
+int factorial(int n);
+
+
+
